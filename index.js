8588171f53734d8d518f471eb9d88aeed35f84c4
Hilary Sbei <hilsbei@gmail.com>
2017-12-12 06:23:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ function happyHolidays(){
   return 'Happy holidays!';
 }
 
-function happyHolidaysTo(name){
-    return `Happy holidays, ${name}!`;
+function happyHolidaysTo(holiday, name){
+  return `Happy holidays, ${name}!`;
+}
+    return `Happy ${holiday}, ${name}!`;
+  }
+
+function holidayCountdown(holiday, days){
+  return `It's ${days} days until ${holiday}!`;
 }
